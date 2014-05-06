e80429c722d6d87f09548b91c12bf129bd708b2e
Koji Ando <miau.jp@gmail.com>
2014-05-06 23:08:22 +0900

    Fix time format in hook.log.

--- ScanSnapHelper/sample_scripts/hook.bat
+++ ScanSnapHelper/sample_scripts/hook.bat
@@ -1,5 +1,5 @@
 cd "%~dp0"
-echo %date%%time% *** hooked %* ***>> hook.log
+echo %date% %time% *** hooked %* ***>> hook.log
 python scripts\copy_here.py %*>> hook.log 2>&1
 ::python scripts\replace_images.py %*>> hook.log 2>&1
 ::python scripts\rotate_for_comic.py %*>> hook.log 2>&1
